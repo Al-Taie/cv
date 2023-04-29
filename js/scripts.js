469886d8b80031947d58158bc14c427f23271c44
Al-Taie <Ahmed.m@the-gw.com>
2023-04-29 14:21:38 +0300

    fix on scroll behavior

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,19 @@
-/*!
-* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-*/
-//
-// Scripts
-//
 
-window.addEventListener("DOMContentLoaded", event => {
+window.addEventListener("scroll", () => {
+    const sections = document.querySelectorAll("section.resume-section");
+    const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
+    let currentSectionIndex = 0;
 
-    // Activate Bootstrap scrollspy on the main nav element
-    const sideNav = document.body.querySelector("#sideNav");
-    if (sideNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: "#sideNav",
-            rootMargin: "0px 0px -40%"
-        });
-    }
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector(".navbar-toggler");
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll("#navbarResponsive .nav-link")
-    );
-    responsiveNavItems.map(function(responsiveNavItem) {
-        responsiveNavItem.addEventListener("click", () => {
-            if (window.getComputedStyle(navbarToggler).display !== "none") {
-                navbarToggler.click();
-            }
-        });
+    sections.forEach((section, index) => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (scrollY >= sectionTop - sectionHeight / 3) {
+            currentSectionIndex = index;
+        }
     });
 
+    navLinks.forEach(link => link.classList.remove("active"));
+    navLinks[currentSectionIndex].classList.add("active");
 });
 
 function revealEmail() {
@@ -38,10 +21,4 @@ function revealEmail() {
     let linkElement = document.getElementById("contact-me");
     linkElement.setAttribute("href", "mailto:" + emailAddress);
     linkElement.textContent = emailAddress;
-}
-
-function changeNavItemColor(element) {
-    const navItems = document.querySelectorAll('.navbar-nav li a');
-    navItems.forEach(item => item.style.color = '');
-    element.style.color = "#f1f1f1";
 }
